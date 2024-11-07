5f3ccb347c208e7fda7e484f2ef172e5c0b9786d
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-11-07 10:52:15 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ const express = require("express")
 
 const app = express();
 
-app.use("/login",(req,res) => {
-    res.send("heloooo");
+app.use("/",(req,res,next) => {
+    const token = "nim";
+    const isAllowed = token === "xyz";
+
+    if(!isAllowed){
+        res.status(404).send("auth error");
+    }
+    else{
+        next();
+    }
 })
 
-app.listen(3000,() => {
+app.get("/user",(req,res) => {
+    res.send("user found successfully");
+})
+
+
+// app.get("/user/ab",(req,res) => {
+//     res.send("helooooo")
+// })
+
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
