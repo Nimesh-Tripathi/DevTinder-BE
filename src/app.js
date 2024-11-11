344cf5fbcd2542ee826476a8844fcbb8e29ddec7
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-11-11 08:56:27 +0530

    CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,28 +1,80 @@
 const express = require("express")
+const { ConnectDB } = require("./config/Database");
+const User = require("./models/user");
 
 const app = express();
 
-app.use("/",(req,res,next) => {
-    const token = "nim";
-    const isAllowed = token === "xyz";
+app.use(express.json())
 
-    if(!isAllowed){
-        res.status(404).send("auth error");
+app.post("/signUp", async (req,res) => {
+
+    const user = new User(req.body)
+
+    try{
+        await user.save();
+        res.send("user added successfully")
+    } catch (err) {
+        res.status(404).send("error saving the user")
     }
-    else{
-        next();
+        
+})  
+
+app.get("/feed", async(req,res) => {
+
+    const userEmail = req.body.emailId;
+
+    try {
+        const all =  await User.find({emailId : userEmail});
+        // const all =  await User.find({});
+        res.send(all)
+    } catch (error) {
+        res.status(404).send("error fetching all users")
     }
 })
 
-app.get("/user",(req,res) => {
-    res.send("user found successfully");
+app.get("/find", async(req,res) => {
+
+    const userID = req.body.id;
+
+    try {
+        const ans = await User.findById(userID);
+        res.send(ans)
+    } catch (error) {
+        res.status(404).send("user not found");
+    }
 })
 
 
-// app.get("/user/ab",(req,res) => {
-//     res.send("helooooo")
-// })
+app.delete("/delete", async(req,res) => {
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    const userId = req.body.id;
+
+    try {
+        await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully")
+    } catch (error) {
+        res.status(404).send("user not found");
+    }
 })
+
+app.patch("/update", async (req,res) => {
+    const userId = req.body.id;
+    const data = req.body;
+
+    try {
+        await User.findByIdAndUpdate(userId,data)
+        res.send("user updated successfully");
+    } catch (error) {
+        res.status(404).send("user not found");
+    }
+})
+
+ConnectDB().then( () => {
+    console.log("Database connected successfully");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    })
+}).catch ( () => {
+    console.log("Database connection failed");
+})
+
