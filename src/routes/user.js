6b0bf2157e0285133ea981986e934e8a57b1cfb8
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-12-04 19:11:50 +0530

    .

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,45 +1,65 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const user = require("../models/user");
 const userRouter = express.Router();
 
 
-userRouter.get("/user/requests/received",userAuth,async(req,res) => {
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try {
-    
+
         const user = req.user;
 
-        const connectionRequest = await ConnectionRequestModel.find({toUserId : user._id, }).populate("fromUserId",["firstName","lastName"]);
+        const connectionRequest = await ConnectionRequestModel.find({ toUserId: user._id, }).populate("fromUserId", ["firstName", "lastName"]);
 
-        res.json({message:"Data fetched successfully",data:connectionRequest})
+        res.json({ message: "Data fetched successfully", data: connectionRequest })
 
     } catch (error) {
-        res.status(404).send("Error: "+ error.message)
+        res.status(404).send("Error: " + error.message)
     }
 })
 
 
-userRouter.get("/user/connections", userAuth, async(req,res) => {
+userRouter.get("/user/connections", userAuth, async (req, res) => {
 
     try {
 
         const user = req.user;
 
         const connectionRequest = await ConnectionRequestModel.find({
-            $or : [
-                { toUserId : user._id, status : "accepted" },
-                {fromUserId : user._id, status : "accepted"},    
+            $or: [
+                { toUserId: user._id, status: "accepted" },
+                { fromUserId: user._id, status: "accepted" },
             ],
-         }).populate("fromUserId",["firstName","lastName"]);
+        }).populate("fromUserId", ["firstName", "lastName"]).populate("toUserId", ["firstName", "lastName"]);
 
-        res.json({data:connectionRequest})
-          
+        const data = connectionRequest.map((row) => {
+            if(row.fromUserId._id.toString() === user._id.toString()){
+                return  row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({ data })
+
     } catch (error) {
-        res.status(404).send("Error: "+ error.message)
+        res.status(404).send("Error: " + error.message)
     }
 })
 
- 
+
+userRouter.get("/find", async (req,res) => {
+    try {
+
+        const data = await user.find({});
+
+        res.send(data);
+        
+    } catch (error) {
+        res.status(404).send("Error: " + error.message)
+    }
+})
+
 
 // authRouter.get("/feed", async (req, res) => {
 
