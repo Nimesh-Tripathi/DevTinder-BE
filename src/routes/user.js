fa91fbac1dd0e2b5b32e877d799c74dd74bebc5f
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-12-07 15:35:30 +0530

    Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -53,6 +53,11 @@ userRouter.get("/feed",userAuth , async (req,res) => {
 
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
         const connectionRequests = await ConnectionRequestModel.find({
             $or: [{fromUserId : loggedInUser._id}, {toUserId : loggedInUser._id}]
         })
